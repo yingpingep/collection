e4bae44614abb086958ade7455bee9de712fa535
yinnping <ciaoing@livemail.tw>
2020-12-20 23:32:30 +0800

    Add HomeComponent and Update routing rule
    Make HomeComponent as a base component to show up @Collection/Intro and demo cards

--- apps/main/src/app/app.module.ts
+++ apps/main/src/app/app.module.ts
@@ -4,10 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RoutingModule } from './routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, RoutingModule, BrowserAnimationsModule],
+  declarations: [AppComponent, HomeComponent],
+  imports: [
+    BrowserModule,
+    RoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- apps/main/src/app/routing.module.ts
+++ apps/main/src/app/routing.module.ts
@@ -1,17 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'me',
-    pathMatch: 'full',
-  },
-  {
-    path: 'me',
-    loadChildren: () =>
-      import('@collection/intro').then((lib) => lib.IntroModule),
+    component: HomeComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'me',
+        pathMatch: 'full',
+      },
+      {
+        path: 'me',
+        loadChildren: () =>
+          import('@collection/intro').then((lib) => lib.IntroModule),
+      },
+    ],
   },
 ];
 
